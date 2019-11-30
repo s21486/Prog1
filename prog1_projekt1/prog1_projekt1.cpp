2a345e80f48d0e4ca823f87245df8ea813740aaa
Błażej Bałęczny <s21486@pjwstk.edu.pl>
2019-11-30 08:17:49 +0100

    zmiana w findPath()

--- prog1_projekt1/prog1_projekt1.cpp
+++ prog1_projekt1/prog1_projekt1.cpp
@@ -1,18 +1,18 @@
 ﻿//autor: Błażej Bałęczny @PJATK 2019
 
-/* Program znajduje najkrótszą ścieżkę, poruszając się po tabeli losowych liczb
- * naturalnych z zakresu 1-25 o wymiarach podanych przez użytkownika. Ścieżka 
- * szukana jest od pierwszej do ostatniej kolumny. Odległość pomiędzy komórkami 
- * jest równa liczbie znajdującej się w komórce docelowej. Przejście jest 
- * możliwe wyłącznie pomiędzy sąsiadującymi komórkami. Za sąsiadujące komórki 
- * uznaje się takie, które leżą w sąsiadujących kolumnach w tym samym wierszu 
- * ("obok") lub w wierszu o indeksie o jeden większym i mniejszym ("na ukos").
- * Punktem początkowym jest abstrakcyjny punkt o wartości 0, który leży na lewo
- * od pierwszej kolumny i sąsiaduje ze wszystkimi komórkami tejże. Po wyliczeniu
- * wartości najkrótszej trasy, program wyświetla odległości poszczególnych 
- * kroków oraz ich sumę.
+/* Program znajduje najkrótszą ścieżkę, poruszając się po tabeli o wymiarach
+ * podanych przez użytkownika, której komórki wypełnione są losowymi liczbami
+ * naturalnymi z zakresu 1-25. Ścieżka szukana jest od pierwszej do ostatniej
+ * kolumny. Odległość pomiędzy komórkami jest równa liczbie znajdującej się
+ * w komórce docelowej. Przejście jest możliwe wyłącznie pomiędzy sąsiadującymi
+ * komórkami. Za sąsiadujące komórki uznaje się takie, które leżą
+ * w sąsiadujących kolumnach w tym samym wierszu ("obok") lub w wierszu
+ * o indeksie o jeden większym i mniejszym ("na ukos"). Punktem początkowym jest
+ * abstrakcyjny punkt o wartości 0, który leży na lewo od pierwszej kolumny
+ * i sąsiaduje ze wszystkimi komórkami tejże. Po wyliczeniu wartości najkrótszej
+ * trasy, program wyświetla odległości poszczególnych kroków oraz ich sumę.
  */
-                                                                                
+
 
 #include <iostream>
 #include <time.h>
@@ -20,22 +20,22 @@
 #include <numeric>
 #include <algorithm>
 
-/* Funkcja zwracająca wskaźnik do dwuwymiarowej tablicy, którą zapełniła 
- * losowymi liczbami naturalnymi z zakresu 1-25.
- */
+ /* Funkcja zwracająca wskaźnik do dwuwymiarowej tablicy, którą zapełniła
+  * losowymi liczbami naturalnymi z zakresu 1-25.
+  */
 int** makeArray(int rows, int columns) {
     srand(time(NULL));
 
     // Wskaźnik do dwuwymiarowej tablicy o dynamicznie alokowanej wielkości
     int** numberMatrix{ new int* [rows] };
     for (int i = 0; i < rows; i++) numberMatrix[i] = new int[columns];
-	
+
     // Pętle wypełniają tablicę losowymi liczbami
-    for (int row = 0; row < rows; row++){
+    for (int row = 0; row < rows; row++) {
         for (int column = 0; column < columns; column++) {
             numberMatrix[row][column] = rand() % 25 + 1;
-		}
-	}
+        }
+    }
     return numberMatrix;
 }
 
@@ -57,90 +57,56 @@ void printArray(int** numbers, int rows, int columns) {
  */
 std::vector<int> findPath(int** numbers, int rows, int columns) {
 
-    /* Vector distance zawiera odległości dojścia do wszystkich kroków 
-     * pośrednich. Jest zbudowany analogicznie do tablicy wejściowej numbers,
-     * jednak każda komórka będzie zawierała dodatkowo zestaw odległości, przez
-     * które funkcja przeszła do tej pory, szukając najkrótszej ścieżki. 
-     */
+    /* Vector distance to vector o trzech poziomach zagnieżdżenia. Jego wielkość
+     * jest tożsama z ilością rzędów tablicy numbers. Ilość vectorów w */
     std::vector<std::vector<std::vector<int>>> distance;
-
-    /* Poniższa pętla wypełnia vector distance danymi początkowymi. W graficznym
-     * przedstawieniu, pierwsza kolumna jest zapełniana tymi samymi liczbami,
-     * co pierwsza kolumna tablicy wejściowej numbers. Pozostałe kolumny są
-     * zapełniane pustymi vectorami liczb.
-     */
     for (int row = 0; row < rows; row++) {
         std::vector<std::vector<int>> rowVector;
-        for (int column = 0; column < columns; column++) {
+        for (int column = 0; column < columns - 1; column++) {
             std::vector<int> cellVector;
-            if (column == 0) cellVector.push_back(numbers[row][column]);
             rowVector.push_back(cellVector);
         }
         distance.push_back(rowVector);
     }
-    int indexA{ -1 }; // Delta indeksu komórki "na ukos w górę"	
-    int indexB{ 0 }; // Delta indeksu komórki "obok"
-    int indexC{ 1 }; // Delta indeksu komórki "na ukos w dół"
-
-    // Pętla sprawdza i zapełnia po kolei puste kolumny tabeli distance.
-    for (int column = 1; column < distance[0].size(); column++) {
-        for (int row = 0; row < distance.size(); row++) {
-            // jeżeli wiersz jest pierwszy, to nie sprawdza wiersza przed nim
-            if (row == 0) {  
-                indexA = 0 ;
-            }
-            // jeżeli wiersz jest ostatni, to nie sprawdza wiersza po nim
-            if (row == distance.size() - 1) {  
-                indexC = 0;
-            }
-            int a{ // Suma odległości z komórki "na ukos w górę"
-                std::accumulate(distance[row + indexA][column - 1].begin(),
-                                distance[row + indexA][column - 1].end(), 0) 
-            }; 
-            int b{ // Suma odległości z komórki "obok"
-                std::accumulate(distance[row + indexB][column - 1].begin(),
-                                distance[row + indexB][column - 1].end(), 0) 
-            }; 
-            int c{ // Suma odległości z komórki "na ukos z dołu"
-                std::accumulate(distance[row + indexC][column - 1].begin(),
-                                distance[row + indexC][column - 1].end(), 0) 
-            }; 
-            int smallest{ std::min(a, std::min(b, c)) };
-
-            // Instrukcja szuka najmniejszej sumy i wskazuje jej indeks.
-            int index;
-            if (smallest == a) { index = indexA; }
-            else if (smallest == b) { index = indexB; }
-            else { index = indexC; }
-
-            // Pętla uzupełnia komórkę najkrótszą drogą do niej prowadzącą.
-            for (int number : distance[row + index][column - 1]) {
-                distance[row][column].push_back(number);
-            }
-            distance[row][column].push_back(numbers[row][column]);
-
-            // Przywrócenie delty do domyślnych wartości.
-            indexA = -1;
-            indexC = 1;
-        }
-    }
-    /* Najkrótsza odkryta droga rozwiązania. Zainicjalizowana na
-     * najwyższą możliwą wartość.
-     */
-    unsigned long long lowestSum{ 25 * distance[0].size() };
-
+    
     std::vector<int> result;
-    /* Pętla sprawdza po kolei komórki ostatniej kolumny vectora distance
-     * i szuka takiej o najniższej sumie wartości.
-     */
-    for (int i = 0; i < distance.size(); i++) {
-        int vectorSum {
-            std::accumulate(distance[i][distance[i].size() - 1].begin(),
-                            distance[i][distance[i].size() - 1].end(), 0)
-        };
-        if (vectorSum < lowestSum) {
-            lowestSum = vectorSum;
-            result = distance[i][distance[i].size() - 1];
+    int resultSum{ 25 * (columns - 1) };
+
+    for (int column = 0; column < columns; column++) {
+        for (int row = 0; row < rows; row++) {
+
+            if (column == 0)
+                distance[row][column].push_back(numbers[row][column]);
+            else {
+                int indexA{ row == 0 ? 0 : -1 };
+                int indexB{ row == rows - 1 ? 0 : 1 };
+                std::vector<int> smallestDistance;
+                for (int index = indexA; index <= indexB; index++) {
+                    std::vector<int> testCase { 
+                        distance[row + index][column - 1] 
+                    };
+                    if (index == indexA) {
+                        smallestDistance = testCase;
+                    }
+                    else {
+                        int testCaseSum{ std::accumulate(testCase.begin(),
+                            testCase.end(), 0) };
+                        int smallestSum{ std::accumulate(smallestDistance.begin(),
+                            smallestDistance.end(), 0) };
+                        if (testCaseSum < smallestSum) smallestDistance = testCase;
+                    }
+                }
+                smallestDistance.push_back(numbers[row][column]);
+                if (column != columns - 1)
+                distance[row][column] = smallestDistance;
+                else {
+                    int lastCellSum{ std::accumulate(smallestDistance.begin(),smallestDistance.end(),0) };
+                    if (lastCellSum < resultSum) {
+                        resultSum = lastCellSum;
+                        result = smallestDistance;
+                    }
+                }
+            }
         }
     }
     return result;
@@ -157,13 +123,13 @@ void interface() {
     int** numbers{ makeArray(rows, columns) };
     printArray(numbers, rows, columns);
     std::vector<int> result{ findPath(numbers, rows, columns) };
-	
+
     // Wyświetlenie wyniku
     for (int number : result) {
         std::cout << number << ' ';
     }
-    std::cout << "\nSUMA: " << std::accumulate(result.begin(), result.end(), 0) 
-              << '\n';
+    std::cout << "\nSUMA: " << std::accumulate(result.begin(), result.end(), 0)
+        << '\n';
 }
 
 
